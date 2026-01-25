5940150cbe55cb855a96a60f6252b107f2dff31a
debil <debil@example.com>
2026-01-25 15:21:51 +0300

    добавил удаление снэпшотов/бекапов старше 14 дней

--- backup-world.sh
+++ backup-world.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-dt=$(date +"%Y-%m-%d %H:%M:%S")
-zip -r "${HOME}/backup ${dt}.zip" ./minecraft_data/world ./minecraft_data/world_nether ./minecraft_data/world_the_end
+# берем время запуска скрипта
+dt=$(date +"%Y-%m-%d-%H-%M-%S")
+echo $dt начинаю бекапирование
+
+# бекапируем миры
+zip -r "minecraft_data/backups/world/${dt}.zip" "minecraft_data/world" 
+zip -r "minecraft_data/backups/world_nether/${dt}.zip" "minecraft_data/world_nether" 
+zip -r "minecraft_data/backups/world_the_end/${dt}.zip" "minecraft_data/world_the_end" 
+
+# удаляем бекапы старше 14 дней
+echo Файлы на удаление: $(find ./minecraft_data/backups/ -type f -mtime +14)
